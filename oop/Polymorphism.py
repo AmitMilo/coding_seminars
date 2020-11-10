accefa20c9009f0e0f8efe0011bd7056de3aab9c
AmitMilo <amit.milo@pagaya.com>
2020-11-10 16:37:09 +0200

    Rechagne in design

--- oop/Polymorphism.py
+++ oop/Polymorphism.py
@@ -12,10 +12,9 @@ class MyLoans(metaclass=ABCMeta):
     def __init__(self, df):
         self.loan_amnt = df["loan_amnt"]
         self.term = df["term"]
-        self.int_rate = None   # Assume this column differs between the platforms.
 
     def calc_revenue(self):
-        return self.loan_amnt * np.power((1 + self.int_rate), self.term) - self.loan_amnt
+        return self.loan_amnt * np.power((1 + self.get_int_rate()), self.term) - self.loan_amnt
 
 
 class MyLCLoans(MyLoans):
@@ -23,11 +22,17 @@ class MyLCLoans(MyLoans):
         super().__init__(df)
         self.int_rate = df["int_rate"]
 
+    def get_int_rate(self):
+        return self.int_rate
+
 
 class MyProsperLoans(MyLoans):
     def __init__(self, df):
         super().__init__(df)
-        self.int_rate = df["intRate"]
+        self.int_rate_str = df["intRate"]
+
+    def get_int_rate(self):
+        return self.int_rate_str.astype(np.float64)
 
 
 def foo(loan: MyLoans):
@@ -35,11 +40,10 @@ def foo(loan: MyLoans):
 
 
 if __name__ == '__main__':
-    df = pd.DataFrame([[1000, 12, 0.3], [2500, 36, 0.1]])
     common_columns = ["loan_amnt", "term"]
-    lc_df = df.copy()
+    lc_df = pd.DataFrame([[1000, 12, 0.3], [2500, 36, 0.1]])
     lc_df.columns = common_columns + ["int_rate"]
-    prosper_df = df.copy()
+    prosper_df = pd.DataFrame([[1000, 12, "0.3"], [2500, 36, "0.1"]])
     prosper_df.columns = common_columns + ["intRate"]
 
     foo(MyLCLoans(lc_df))
